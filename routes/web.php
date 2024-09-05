968f3d1efb82ea14add221c6e3de97e648ad19c7
Katja <you@example.com>
2024-09-05 14:47:33 +0300

    half complete app

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlaylistController;
+
+Route::get('playlists', [PlaylistController::class, 'index'])->name('playlists.index'); // Show all playlists
+Route::get('playlists/create', [PlaylistController::class, 'create'])->name('playlists.create'); // Show form to create a new playlist
+Route::post('playlists', [PlaylistController::class, 'store'])->name('playlists.store'); // Store new playlist
+Route::get('playlists/{playlist}', [PlaylistController::class, 'show'])->name('playlists.show'); // Show single playlist
+Route::get('playlists/{playlist}/edit', [PlaylistController::class, 'edit'])->name('playlists.edit'); // Edit a playlist
+Route::put('playlists/{playlist}', [PlaylistController::class, 'update'])->name('playlists.update'); // Update playlist
+Route::delete('playlists/{playlist}', [PlaylistController::class, 'destroy'])->name('playlists.destroy'); // Delete a playlist
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
